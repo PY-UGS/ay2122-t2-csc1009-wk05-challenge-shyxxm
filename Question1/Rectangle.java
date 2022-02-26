f3974022ed960e8b4746924a20d0e8c20a53869e
shyxxm <48471694+shyxxm@users.noreply.github.com>
2022-02-26 21:29:32 +0800

    Add files via upload

---
+++ Question1/Rectangle.java
@@ -0,0 +1,45 @@
+public class Rectangle extends GeometricObject {
+    private double width;
+    private double height;
+
+    public Rectangle(){
+
+    }
+
+    public Rectangle(double width, double height){
+        this.width =  width;
+        this.height = height;
+    }
+
+    public Rectangle(double width, double height, String color, Boolean filled){
+        //this.width = width;
+        //this.height = height;
+        //this.setColor(color);
+        //this.setFilled(filled);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea(){
+        return height * width;
+    }
+
+    public double getPerimeter(){
+        return ((height*2) + (width*2));
+    }
+
+}
